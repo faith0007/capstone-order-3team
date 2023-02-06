454444798086c3a33b3c0f67ef019e748c6ae7ae
faith0007 <faith0007@hotmail.com>
2023-02-06 06:30:25 +0000

    teset commit

--- delivery/src/test/java/capstoneorderteam/main/TestMain.java
+++ delivery/src/test/java/capstoneorderteam/main/TestMain.java
@@ -2,12 +2,19 @@ package capstoneorderteam.main;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+
+import java.beans.Transient;
+
 import org.junit.runner.RunWith;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin={"pretty","html:target/cucumber"},
                 features = "src/test/resources/features",
                 extraGlue="capstoneorderteam/common")
 public class TestMain {
-    
+
+    public void test001(){
+
+    };
 }
